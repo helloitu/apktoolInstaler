8c33f228e0968860576e7ab22db0ac0cc1aa5ab6
Root <whiterabbit@whiterabbit-X451CA.(none)>
2015-07-08 13:24:34 -0300

    Inicio do script

---
+++ instalar.sh
@@ -0,0 +1,11 @@
+sudo apt-get install figlet
+figlet SmoB
+echo -----------------------------------------------
+echo     Desenvolvido By SmoB Tutoriais 
+echo -----------------------------------------------
+sudo chmod +x apktool
+sudo cp apktool apktool.jar /usr/local/bin
+cd 
+echo -----------------------------------------------
+echo   Instalacao Finalizada 
+echo -----------------------------------------------
